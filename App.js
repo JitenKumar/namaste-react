78f1d88b3d95423dc6ca5c71e2e469529af5c584
b0224761 <jitender3.kumar@airtel.com>
2024-06-14 13:30:27 +0530

    Basic learning

--- App.js
+++ App.js
@@ -1,30 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// Using JSX for write the code to make life easier
+//JSX is not like HTML, but it is HTML like syntax
+// it is more of like XML syntax
+//JSX is just syntax
+/* JSX is not understood by the JSEngine, it is //
+    transpiled by Parcel first using the Babel and then convert the code to one understood by JSEngine
+*/
 /**
- * <div parent>
- * <div id=child1>
-    * <h1 id=h1></h1>
-    * <h1 id=h2></h1>
- * <div id=child2>
-      * <h1 id=h1></h1>
-      * <h1 id=h2></h1>
- * </div>
+ * JSX  => BABEL => React.createElement => Now React Understand it and then conver to => HTML Element => JSEgine
  */
+/**
+ * React Element
+ */
+const jsxHeading = <h1 className="head">This is JSX Title Heading</h1>;
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading1" }, "This is heading1"),
-    React.createElement("h2", { id: "heading2" }, "This is heading2"),
-  ])
-);
-
-//JSX for above issues
-
-// const h1 = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello world from React"
-// );
-console.log(parent);
+/**
+ * React Component
+ * 1. Class Based Component -> Old React Component
+ * 2. Function Based Component -> New React Component
+ */
+const title = <h1 className="title">This is React Element</h1>;
+const Title = () => {
+  {
+    title;
+  }
+  return <h1 className="head">This is JSX function Title</h1>;
+};
+// Component Compositions -> composing two component
+const Heading = () => {
+  return (
+    <div id="container">
+        {Title()}
+        <Title></Title>
+      <Title />
+      <h1 className="head">This is JSX function heading componet</h1>;
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<Heading />);
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # namaste-react
 namaste-react
+# parcel
+Dev Building
+Local server
+HMR : Hot module replacement
+Parcel uses : file watching algorithm - which is written in c++
+Faster builds : due to caching
+Image optimization for faster loading of images
+Bundle of files 
+Minify and compres the files
+Consistent hashing 
+Code Splitting
+Differential Bundling to support older browsers
+Diagnostign behind the scenes for error messages and optimization
+Also provides support for hosting on HTTPS instead of http
+Tree Shaking algorithm - remove unused code or functions if not used
+different DEV and PROD BUILD
+
+# React Basics
+Using JSX for write the code to make life easier
+JSX is not like HTML, but it is HTML like syntax
+It is more of like XML syntax
+JSX is just syntax
+/* JSX is not understood by the JSEngine, it is //
+//transpiled by Parcel first using the Babel and then convert the code to one understood by JSEngine
+*/
+/**
+ JSX  => BABEL => React.createElement => Now React Understand it and then conver to => HTML Element => JSEgine
+ */
+ JSX handles the Cross Site Scripting and escapes all these javascript executons
